38d3fc717df712f90a9147f8b85140dec2dece93
Sergej Kukharev <kukharev@narod.ru>
2024-12-13 10:55:49 +0300

    Устранение замечаний ревью - Для упрощения копипасты рекомендую использовать: Array(repeating: "Text1", count: 9).joined(separator: " ")

--- TravelPlanner/Services/StoryFactory.swift
+++ TravelPlanner/Services/StoryFactory.swift
@@ -17,56 +17,56 @@ final class StoryFactory: ObservableObject {
         let story1 = Story(
             previewImage: Asset.Images.Stories.story1Preview.image,
             image: Asset.Images.Stories.story1Detail1.image,
-            title: "Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1",
-            description: "Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text"
+            title: Array(repeating: "Text1", count: 9).joined(separator: " "),
+            description: Array(repeating: "Text", count: 20).joined(separator: " ")
         )
         let story2 = Story(
             previewImage: Asset.Images.Stories.story2Preview.image,
             image: Asset.Images.Stories.story2Detail1.image,
-            title: "Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2",
-            description: "Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text"
+            title: Array(repeating: "Text2", count: 9).joined(separator: " "),
+            description: Array(repeating: "Text", count: 20).joined(separator: " ")
         )
         let story3 = Story(
             previewImage: Asset.Images.Stories.story3Preview.image,
             image: Asset.Images.Stories.story3Detail1.image,
-            title: "Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3",
-            description: "Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text"
+            title: Array(repeating: "Text3", count: 9).joined(separator: " "),
+            description: Array(repeating: "Text", count: 20).joined(separator: " ")
         )
         let story4 = Story(
             previewImage: Asset.Images.Stories.story4Preview.image,
             image: Asset.Images.Stories.story4Detail1.image,
-            title: "Text4 Text4 Text4 Text4 Text4 Text4 Text4 Text4 Text4",
-            description: "Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text"
+            title: Array(repeating: "Text4", count: 9).joined(separator: " "),
+            description: Array(repeating: "Text", count: 20).joined(separator: " ")
         )
         let story5 = Story(
             previewImage: Asset.Images.Stories.story5Preview.image,
             image: Asset.Images.Stories.story5Detail1.image,
-            title: "Text5 Text5 Text5 Text5 Text5 Text5 Text5 Text5 Text5",
-            description: "Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text"
+            title: Array(repeating: "Text5", count: 9).joined(separator: " "),
+            description: Array(repeating: "Text", count: 20).joined(separator: " ")
         )
         let story6 = Story(
             previewImage: Asset.Images.Stories.story6Preview.image,
             image: Asset.Images.Stories.story6Detail1.image,
-            title: "Text6 Text6 Text6 Text6 Text6 Text6 Text6 Text6 Text6",
-            description: "Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text"
+            title: Array(repeating: "Text6", count: 9).joined(separator: " "),
+            description: Array(repeating: "Text", count: 20).joined(separator: " ")
         )
         let story7 = Story(
             previewImage: Asset.Images.Stories.story7Preview.image,
             image: Asset.Images.Stories.story7Detail1.image,
-            title: "Text7 Text7 Text7 Text7 Text7 Text7 Text7 Text7 Text7",
-            description: "Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text"
+            title: Array(repeating: "Text7", count: 9).joined(separator: " "),
+            description: Array(repeating: "Text", count: 20).joined(separator: " ")
         )
         let story8 = Story(
             previewImage: Asset.Images.Stories.story8Preview.image,
             image: Asset.Images.Stories.story8Detail1.image,
-            title: "Text8 Text8 Text8 Text8 Text8 Text8 Text8 Text8 Text8",
-            description: "Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text"
+            title: Array(repeating: "Text8", count: 9).joined(separator: " "),
+            description: Array(repeating: "Text", count: 20).joined(separator: " ")
         )
         let story9 = Story(
             previewImage: Asset.Images.Stories.story9Preview.image,
             image: Asset.Images.Stories.story9Detail1.image,
-            title: "Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9",
-            description: "Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text"
+            title: Array(repeating: "Text9", count: 9).joined(separator: " "),
+            description: Array(repeating: "Text", count: 20).joined(separator: " ")
         )
 
         self.stories = [story1, story2, story3, story4, story5, story6, story7, story8, story9]
